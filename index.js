a3c14bb6d3c3287f660aae004bf599cd49012b2b
mrh9ud <mrh9ud@virginia.edu>
2019-12-18 12:30:05 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,67 @@
 // Add your code here
+let formData = {
+    dogName: "Byron",
+    dogBreed: "Poodle"
+}
+
+let configurationObject = {
+    method: "POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+    },
+    body: JSON.stringify(formData)
+}
+fetch('http://localhost:3000/dogs', configurationObject)
+    .then(function(response) {
+        return response.json()
+    })
+    .then(function(object) {
+        console.log(object)
+    })
+    .catch(function(error) {
+        alert("Bad things! Ragnarok!")
+        console.log(error.message)
+})
+
+
+const url = "https://data.cityofnewyork.us/api/views/p94q-8hxh"
+
+function getJSON(url) {
+    return fetch(url).then(response => response.json())
+}
+
+getJSON(url).then(console.log)
+
+function submitData(userName, userEmail) {
+    let data = {
+        name: userName,
+        email: userEmail
+    }
+
+    let configObject = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json"
+        },
+        body: JSON.stringify(data)
+    }
+    return fetch('http://localhost:3000/users', configObject)
+        .then( function(response) {
+            return response.json()
+        })
+        .then( function(object) {
+            document.body.innerHTML = object['id']
+        })
+        .catch( function(error) {
+            document.body.innerHTML = error.message
+        })
+}
+
+function appendToDOM(userInfo) {
+    console.log('testing')
+}
+
+submitData('matt', 'matt email')
+
